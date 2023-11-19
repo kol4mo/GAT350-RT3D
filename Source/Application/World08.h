f1fc3bda54a661e4ec0b0b22182988f242924ea7
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2023-11-18 18:58:18 -0700

    added cel shader

---
+++ Source/Application/World08.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Framework/World.h"
+#include "Renderer/Renderer.h"
+#include "Core/Math/Transform.h"
+#include <vector>
+
+
+namespace nc
+{
+
+	class World08 : public World
+	{
+	public:
+		const uint32_t INVERT_MASK = (1<<0);
+		const uint32_t GRAYSCALE_MASK = (1<<1);
+		const uint32_t COLORTINT_MASK = (1<<2);
+		const uint32_t GRAIN_MASK = (1<<3);
+		const uint32_t SCANLINE_MASK = (1<<4);
+		const uint32_t KERNEL_MASK = (1<<5);
+		const uint32_t BLUR_MASK = (1<<6);
+
+	public:
+		bool Initialize() override;
+		void Shutdown() override;
+		void Update(float dt) override;
+		void Draw(Renderer& renderer) override;
+
+	private:
+		float m_time = 0;
+		float m_speed = 5;
+		float m_blend{ 1 };
+		int m_celLevels = 4;
+		float m_specularCut = 0.05f;
+		uint32_t m_params{ 0 };
+		glm::vec4 m_colorTint{ 1,1,1, 1 };
+		
+		float m_refraction{ 1 };
+		glm::vec3 lightAColor = { 0.2, 0.2, 0.2 };
+
+
+	};
+}
